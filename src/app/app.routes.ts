6d2fc2d1340e8ac3d3c42a7ddc2055f7db762175
Angelo Meza <meza1044@gmail.com>
2025-02-10 01:52:43 -0500

    Configuraciones de estilos y modificaciones al proyecto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CompanyListComponent } from './features/companies/company-list/company-list.component';
+import { CustomerListComponent } from './features/customers/customer-list/customer-list.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'companies', component: CompanyListComponent },
+    { path: 'customers', component: CustomerListComponent },
+    { path: '', redirectTo: '/companies', pathMatch: 'full' }
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule { }
